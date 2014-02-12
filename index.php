e136b15916ae420238a2dca2411fdf7af38eacb9
Chocobozzz <djidane14ff@hotmail.fr>
2014-02-12 10:44:00 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+    session_start();
+    
+    if(isset($_GET['deconnexion'])){
+        session_destroy();
+        header("Location: .");
+    }
+    
+    // Tentative de connexion ?
+    if(isset($_POST['id'], $_POST['pass'])){
+        require('connexion_bdd.php');
+        
+        $req = $bdd->prepare('SELECT * FROM admin WHERE admin_id = ? AND admin_pass = ?');
+        $req->execute(array($_POST['id'], sha1($_POST['pass'])));
+        
+        if($data = $req->fetch()){
+            $_SESSION['admin_id'] = $data['admin_id'];
+            $connexion = true;
+        }
+        else{
+            $connexion = false;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        
+        <link rel="stylesheet" href="css/slick.grid.css" type="text/css"/>
+        <link rel="stylesheet" href="css/slick-default-theme.css" type="text/css"/>
+        <link rel="stylesheet" href="css/jquery-ui-1.8.16.custom.css" type="text/css"/>
+        <link rel="stylesheet" href="css/index.css" type="text/css"/>
+    </head>
+    <body>
+        <?php
+            // Si pas connecté on affiche le formulaire
+            if(!isset($_SESSION['admin_id'])){
+        ?>
+                <div id="bloc_connexion">
+                    <form id="form_connexion" method="POST">
+                        <label for="id">Pseudo :</label>
+                        <input type="text" id="id" name="id" />
+                        
+                        <br /><br />
+                        
+                        <label for="pass">Mot de passe :</label>
+                        <input type="password" id="pass" name="pass" />
+                        
+                        <br /><br />
+                        
+                        <input id="connexion" name="connexion" type="submit" value="Connexion" />
+                    </form>
+                </div>
+        <?php
+            }
+            // Sinon on y met le javascript et les grilles
+            else{
+        ?>
+                <div id="presentation_administrateur">
+                    Administrateur : <?php echo $_SESSION['admin_id']; ?> / <a href="index.php?deconnexion" title="Se déconnecter ?">Déconnexion ?</a>
+                </div>
+                
+                <div>
+                    <div class="grid-header">
+                      <label>Users</label>
+                    </div>
+                    <div class="grid" id="grid_user"></div>
+                </div>
+                
+                <div>
+                    <div class="grid-header">
+                      <label>Logs</label>
+                    </div>
+                     <div class="grid" id="grid_log"></div>
+                </div>
+                
+                <div>
+                    <div class="grid-header">
+                      <label>Admin</label>
+                    </div>
+                    <div class="grid" id="grid_admin"></div>
+                </div>
+                
+               
+                <script src="js/jquery-1.7.min.js"></script>
+                <script src="js/jquery-ui-1.8.16.custom.min.js"></script>
+                <script src="js/jquery.event.drag-2.2.js"></script>
+                <script src="js/slick.core.js"></script>
+                
+                
+                <script src="js/slick.formatters.js"></script>
+                <script src="js/slick.editors.js"></script>
+                <script src="js/slick.grid.js"></script>
+                <script src="js/index.js"></script>
+                
+        <?php
+            }
+        ?>
+        
+        
+        
+    </body>
+</html>
