96e884c1dcf5a6c162f06526c4eef1e84218e077
Paul Rock <paul@drteam.rocks>
2018-01-30 20:02:01 +0300

    certs available for read now

--- scripts/install-openvpn.sh
+++ scripts/install-openvpn.sh
@@ -108,6 +108,7 @@ printf "\n################## Setup OpenVPN ##################\n"
 
 # Copy certificates and the server configuration in the openvpn directory
 cp /etc/openvpn/easy-rsa/pki/{ca.crt,ta.key,issued/server.crt,private/server.key,dh.pem} "/etc/openvpn/"
+chmod +r /etc/openvpn/{ca.crt,ta.key}
 cp "$base_path/../configs/server.conf" "/etc/openvpn/"
 mkdir -p "/etc/openvpn/ccd"
 sed -i "
