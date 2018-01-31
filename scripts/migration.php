38a5eae91e57957f1077cc0293cae3218c5de2b9
Paul Rock <paul@drteam.rocks>
2018-01-31 04:06:35 +0300

    paths of migration changed

--- scripts/migration.php
+++ scripts/migration.php
@@ -1,55 +1,51 @@
 <?php
 
-  if (count($argv) !== 2) {
+if (count($argv) !== 2) {
     echo "Need the www base path as argument";
     exit(0);
-  }
+}
 
-  $www = $argv[1];
+$www = $argv[1];
 
-  require("$www/include/config.php");
-  require("$www/include/connect.php");
-  require("$www/include/functions.php");
+require("$www/app/config.php");
+require("$www/app/connect.php");
+require("$www/app/functions.php");
 
-  $migrations = getMigrationSchemas();
+$migrations = getMigrationSchemas();
 
-  try {
+try {
     $req = $bdd->prepare('SELECT `sql_schema` FROM `application` LIMIT 1');
     $req->execute();
     $data = $req->fetch();
 
     $sql_schema = -1;
     if ($data['sql_schema']) {
-      $sql_schema = $data['sql_schema'];
+        $sql_schema = $data['sql_schema'];
     }
-  }
-  // Table does not exist
-  catch (Exception $e) {
+} // Table does not exist
+catch (Exception $e) {
     $sql_schema = -1;
-  }
+}
 
-  // For each migrations
-  foreach ($migrations as $migration_value) {
+// For each migrations
+foreach ($migrations as $migration_value) {
 
     // Do the migration, we are behind the last schema
     if ($sql_schema < $migration_value) {
 
-      // Create the tables or die
-      $sql_file = dirname(__FILE__) . "/sql/schema-$migration_value.sql";
-      try {
-        $sql = file_get_contents($sql_file);
-        $bdd->exec($sql);
-      }
-      catch (PDOException $e) {
-        printError($e->getMessage());
-        exit(1);
-      }
+        // Create the tables or die
+        $sql_file = dirname(__FILE__) . "/sql/schema-$migration_value.sql";
+        try {
+            $sql = file_get_contents($sql_file);
+            $bdd->exec($sql);
+        } catch (PDOException $e) {
+            printError($e->getMessage());
+            exit(1);
+        }
 
-      // Update schema to the new value
-      updateSchema($bdd, $migration_value);
+        // Update schema to the new value
+        updateSchema($bdd, $migration_value);
 
-      echo "Moved to schema $migration_value\n";
+        echo "Moved to schema $migration_value\n";
     }
-  }
-
-?>
+}
