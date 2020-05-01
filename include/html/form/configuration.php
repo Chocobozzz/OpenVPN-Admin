b3b69687732f15ee7511315bf0d907d7148a2cc7
Armin Gorji <arvage@yahoo.com>
2020-05-01 16:16:34 -0700

    added instruction button

--- include/html/form/configuration.php
+++ include/html/form/configuration.php
@@ -18,6 +18,8 @@
           <input type="password" id="configuration_pass" name="configuration_pass" class="form-control" />
         </div>
         <input id="configuration_get" name="configuration_get" type="submit" value="Get configuration files" class="btn btn-default" />
+        <input id="instruction_get" name="instruction_get" type="submit" value="Get Instructions" class="btn btn-default" />
+
       </div>
 
     </form>
