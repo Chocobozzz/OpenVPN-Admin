6c330d2388908f43277b3cc55ed810d8aa85b065
Armin Gorji <arvage@yahoo.com>
2020-05-04 19:27:28 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -59,8 +59,8 @@ fi
 echo -e "${Green}\nAutomated Installation Started\n"
 sleep 2
 
-
-read -t 10 -p "Provide your public IP/Hostname [Detected IP: $public_ip]: " public_hostname
+echo -e "${Red}$public_ip detected as your Public IP and will be used automatically if you don't choose anything else.\nTimeout: 60 Seconds"
+read -t 60 -p "Need to use another public IP/Hostname? Type it here: " public_hostname
 if [ -z "$public_hostname" ]
 then
   public_ip=$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')
