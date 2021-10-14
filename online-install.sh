fa77b962f5a8b649d1690e628ada8f117a2bdeaa
Armin Gorji <arvage@yahoo.com>
2021-10-14 09:50:54 -0700

    Update online-install.sh

--- online-install.sh
+++ online-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+NC='\033[0m'            # No Color
+Red='\033[1;31m'        # Light Red
+Yellow='\033[0;33m'     # Yellow
+Green='\033[0;32m'      # Green
 clear
-echo -e "${Green}Updating and Getting Ready"
+echo -e "${Green}Updating and Getting Ready${NC}"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git
 cd ~
