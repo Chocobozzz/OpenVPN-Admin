28ac14bade38fdddfecd60acca322d1f4f6b04ab
Armin Gorji <arvage@yahoo.com>
2021-10-14 10:00:14 -0700

    Update online-install.sh

--- online-install.sh
+++ online-install.sh
@@ -4,11 +4,13 @@ Red='\033[1;31m'        # Light Red
 Yellow='\033[0;33m'     # Yellow
 Green='\033[0;32m'      # Green
 clear
-echo -e "${Green}Updating and Getting Ready${NC}"
+echo -e "${Green}Updating and Getting Ready${Yellow}"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git
 cd ~
+echo -e "${Red}"
 git clone https://github.com/arvage/OpenVPN-Admin openvpn-admin
+echo -e "${NC}"
 cd openvpn-admin
 chmod +x ./install.sh
 sudo ./install.sh /var/www www-data www-data
