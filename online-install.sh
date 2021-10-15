9aab409e44cd7b3820c6cd9d3d699d24fc62a3af
Armin Gorji <arvage@yahoo.com>
2021-10-15 09:13:44 -0700

    Update online-install.sh

--- online-install.sh
+++ online-install.sh
@@ -4,6 +4,14 @@ Red='\033[1;31m'        # Light Red
 Yellow='\033[0;33m'     # Yellow
 Green='\033[0;32m'      # Green
 clear
+OS=$(cat /etc/os-release | grep PRETTY_NAME | sed 's/"//g' | cut -f2 -d= | cut -f1 -d " ") # Don't change this unless you know what you're doing
+if [[ "$OS" != "Ubuntu" ]]; then
+  echo -e "${Red}Oops! Only Ubuntu and Raspbian OS are supported.${NC}"
+  exit
+elif [[ "$OS" != "Raspbian" ]]; then
+  echo -e "${Red}Oops! Only Ubuntu and Raspbian OS are supported.${NC}"
+  exit
+fi
 echo -e "${Green}Updating and Getting Ready${Yellow}"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git
